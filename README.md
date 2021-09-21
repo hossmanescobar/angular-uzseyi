5bb17770e721116e48b566d387319281c65e935f
hossmanescobar <hossman.escobar@usach.cl>
2021-09-21 11:09:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zfg2ed
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zfg2ed)
---
+++ src/app/select-overview-example.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+/**
+ * @title Basic select
+ */
+@Component({
+  selector: 'select-overview-example',
+  templateUrl: 'select-overview-example.html',
+})
+export class SelectOverviewExample {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
